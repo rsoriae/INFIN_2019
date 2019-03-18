7e2f3c642689b1aafefc6c3db4ffd174ea88eee0
alumne <alumne@lae2005.(none)>
2019-03-18 19:55:22 +0100

    servidor 180319 cosa

--- TCP_servidor/tcpservidor_main.c
+++ TCP_servidor/tcpservidor_main.c
@@ -1,32 +1,111 @@
-/*
- * XXXXXX.c
- * 
- * Copyright 2019 INFIN (EUSS) <euss@euss.cat>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
- */
 
+
+/***************************************************************************
+                          main.c  -  server
+                             -------------------
+    begin                : lun feb  4 15:30:41 CET 2002
+    copyright            : (C) 2002 by A. Moreno
+    email                : amoreno@euss.es
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
 
-int main(int argc, char **argv)
+#define SERVER_PORT_NUM		5001
+#define SERVER_MAX_CONNECTIONS	4
+#define REQUEST_MSG_SIZE	1024
+
+
+/************************
+*
+*
+* tcpServidor
+*
+*
+*/
+
+int main(int argc, char *argv[])
 {
+	struct sockaddr_in	serverAddr;
+	struct sockaddr_in	clientAddr;
+	int			sockAddrSize;
+	int			sFd;
+	int			newFd;
+	int			nRead;
+	int 		result;
+	char		buffer[256];
+	char		missatge[] = "#(2)(0,23.3)(10,23.3)";
+
+	/*Preparar l'adreça local*/
+	sockAddrSize=sizeof(struct sockaddr_in);
+	bzero ((char *)&serverAddr, sockAddrSize); //Posar l'estructura a zero
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(SERVER_PORT_NUM);
+	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	/*Crear un socket*/
+	sFd=socket(AF_INET, SOCK_STREAM, 0);
 	
-	return 0;
+	/*Nominalitzar el socket*/
+	result = bind(sFd, (struct sockaddr *) &serverAddr, sockAddrSize);
+	
+	/*Crear una cua per les peticions de connexió*/
+	result = listen(sFd, SERVER_MAX_CONNECTIONS);
+	
+	/*Bucle d'acceptació de connexions*/
+	while(1){
+		printf("\nServidor esperant connexions\n");
+
+		/*Esperar conexió. sFd: socket pare, newFd: socket fill*/
+		newFd=accept(sFd, (struct sockaddr *) &clientAddr, &sockAddrSize);
+		printf("Connexión acceptada del client: adreça %s, port %d\n",	inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+
+		/*Rebre*/
+		result = read(newFd, buffer, 1024);
+		printf("Missatge rebut del client(bytes %d): %s\n",	result, buffer);
+		
+		//char inici_trama;
+        //esult = read(newFd, buffer, 1);
+        if ((result == 2) && (buffer[0] == 'A')) {
+            //char comanda;
+            //result = read(newFd, &comanda, 2);
+           // if (result == 2) {
+                switch (buffer[1]) {
+                case 'M':
+                    printf("cosa");
+                    break;
+				}
+			}
+          }
+		
+		/*Enviar*/
+		strcpy(buffer,missatge); //Copiar missatge a buffer
+		result = write(newFd, buffer, strlen(buffer)+1); //+1 per enviar el 0 final de cadena
+		printf("Missatge enviat a client(bytes %d): %s\n",	result, missatge);
+
+		/*Tancar el socket fill*/
+		result = close(newFd);
+	}
 }
+
 
